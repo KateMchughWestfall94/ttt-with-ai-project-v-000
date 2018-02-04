33ad3b5f3dd6b9d87bef60750504bcb92805da50
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-04 19:12:10 +0000

    automatically backed up by learn

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -6,7 +6,7 @@ class CLI
     menu
   end
 
-  def menu 
+  def menu
     puts "0 - Computer Vs Computer"
     puts "1 - You Vs Computer"
     puts "2 - You Vs A Friend (or Yourself!)"
@@ -23,18 +23,39 @@ class CLI
     when "2"
       human_vs_human_game
     when "exit"
-      exit 
-    else 
+      exit
+    else
       puts" Please enter a correct option"
-      menu 
+      menu
     end
   end
 
+  #creates a new game with two computer players
+  #play_again method restarts CLI
   def computer_vs_computer_game
     game = Game.new
     puts "This game determines which of our AI Overlords wins Earth "
     Game.new(Players::Computer.new('X'), Players::Computer.new('O')).play
     play_again?
   end
-  
+  #creates a game with one human and one computer
+  #human should pick token
+  def human_vs_computer_game
+    game = nil
+    puts "Get three in a row to win!"
+    puts " "
+    puts "Do you want to be X? Enter y/n"
+    selection = gets.strip
+    if selection == "y"
+      game = Game.new(Players::Human.new("X"), Players::Computer.new("O"))
+    elsif selection == "n"
+      game = Game.new(Players::Computer.new("X"), Players::Human.new("O"))
+    else
+      puts "Please enter a valid response"
+      selection = gets.strip
+    end
+    game.play
+    play_again?
+  end
+
 end
