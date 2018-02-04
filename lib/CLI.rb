7c62e1d4f0010866dd1a929467bcb587159ae608
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-04 19:01:53 +0000

    automatically backed up by learn

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -4,4 +4,37 @@ class CLI
     puts " "
     puts "Please select a play style from the Menu"
     menu
-    
+  end
+
+  def menu 
+    puts "0 - Computer Vs Computer"
+    puts "1 - You Vs Computer"
+    puts "2 - You Vs A Friend (or Yourself!)"
+    puts "exit - leave the Tic-tac-toe game"
+    puts " "
+    puts "Enter Your Selection Below"
+    selection = gets.strip
+
+    case(selection)
+    when "0"
+      computer_vs_computer_game
+    when "1"
+      human_vs_computer_game
+    when "2"
+      human_vs_human_game
+    when "exit"
+      exit 
+    else 
+      puts" Please enter a correct option"
+      menu 
+    end
+  end
+
+  def computer_vs_computer_game
+    game = Game.new
+    puts "This game determines which of our AI Overlords wins Earth "
+    Game.new(Players::Computer.new('X'), Players::Computer.new('O')).play
+    play_again?
+  end
+  
+end
