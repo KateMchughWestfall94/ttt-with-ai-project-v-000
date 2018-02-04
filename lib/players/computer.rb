380ecca62fb7adeef45f3dca12ee65db8f86fe94
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-04 18:22:07 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,8 +1,31 @@
 module Players
   class Computer < Player
     def move(board)
+      if board.cells[4] == " "
+        "5"
+      elsif board.cells[0] == " " || board.cells[2] == " " || board.cells[6] == " " || board.cells[8] == " "
+        choices = ["1", "3", "7", "9"]
+        choice = choices.sample
+        if board.taken?(choice) == false
+          choice
+        else
+          move
+        end
+      else
+      choices = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+      choice = choices.sample
+      if board.taken?(choice) == false
+        choice
+      else
+        move
+      end
+    end
+  end
+      
+
+      
       
     end
-      
+
   end
 end
